dd9eee65528b2de9a4673509394e1f2b2f03ceeb
sadtoru <daiarocio@outlook.com>
2025-02-21 16:25:08 -0300

    estructura del proyecto

--- app/src/main/java/com/example/blogcat/data/model/Post.kt
+++ app/src/main/java/com/example/blogcat/data/model/Post.kt
@@ -1,4 +1,15 @@
 package com.example.blogcat.data.model
 
-class Post {
-}
+data class Post(
+    //@Exclude @JvmField
+    //var id: String = "",
+    //@ServerTimestamp
+    //var created_at: Date? = null,
+    val post_image: String = "",
+    val profile_picture: String = "",
+    val post_description: String = "",
+    //val poster: Poster? = null,
+    //val likes: Long = 0,
+    //@Exclude @JvmField
+    //var liked: Boolean = false)
+)
--- app/src/main/java/com/example/blogcat/ui/home/adapter/HomeScreenAdapter.kt
+++ app/src/main/java/com/example/blogcat/ui/home/adapter/HomeScreenAdapter.kt
@@ -1,6 +1,37 @@
 package com.example.blogcat.ui.home.adapter
 
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.blogcat.core.BaseViewHolder
+import com.example.blogcat.data.model.Post
+import com.example.blogcat.databinding.PostItemViewBinding
 
-class HomeScreenAdapter(): RecyclerView.Adapter<BaseViewHolder>() {
+class HomeScreenAdapter(private val postList: List<Post>): RecyclerView.Adapter<BaseViewHolder<*>>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder<*> {
+        val itemBinding =
+            PostItemViewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return HomeScreenViewHolder(itemBinding, parent.context)
+    }
+
+    override fun getItemCount(): Int = postList.size
+
+    override fun onBindViewHolder(holder: BaseViewHolder<*>, position: Int) {
+        when (holder) {
+            is HomeScreenViewHolder -> holder.bind(postList[position])
+        }
+    }
+
+    private inner class HomeScreenViewHolder(val binding: PostItemViewBinding, val context: Context) :
+        BaseViewHolder<Post>(binding.root) {
+        override fun bind(item: Post) {
+            Glide.with(context).load(item.post_image).centerCrop().into(binding.profilePicture)
+            Glide.with(context).load(item.profile_picture).centerCrop().into(binding.profilePicture)
+            binding.profileName.text = item.post_description
+            binding.postTimestamp.text = "hace 2 horas"
+        }
+
+    }
 }
